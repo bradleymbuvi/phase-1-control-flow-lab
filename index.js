5eae96ede9ef7951fa4dd6be357605736e8c271b
bradleymbuvi <bradwent@gmail.com>
2023-12-15 12:54:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+
   // Write your code here!
+
+const scuberGreetingForFeet = (ride)=> {
+  if (ride <= 400){
+    return 'This one is on me!';
+  }
+  else if(ride >2000 && ride< 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  }
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
   // Write your code here!
+
+const ternaryCheckCity = (city) =>{
+  if (city === 'NYC'){
+      return 'Ok, sounds good.'
+  }
+  else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+
   // Write your code here!
+
+const switchOnCharmFromTip = (tip)=>{
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+  }
+  else if (tip === 'not as generous'){
+    return 'Thank you.'
+  }
+  return 'Bye.'
 }
